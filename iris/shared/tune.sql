68c62c285fc6ce17cacedc43eea68e0419bc578c
Alberto Fuentes <alberto.fuentes@intersystems.com>
2026-04-01 13:14:24 +0200

    add tune sql script

---
+++ iris/shared/tune.sql
@@ -0,0 +1,8 @@
+TUNE TABLE Terminology_Snomed.Concept;
+TUNE TABLE Terminology_Snomed.Description;
+TUNE TABLE Terminology_Snomed.Relationship;
+TUNE TABLE Terminology_Snomed.RefsetMember;
+TUNE TABLE Terminology_Snomed.PreferredTerm;
+TUNE TABLE Terminology_Snomed.IsaClosure;
+TUNE TABLE Terminology_Snomed.PreferredTermStage;
+TUNE TABLE Terminology_Snomed.LanguageRefSetConfig;
